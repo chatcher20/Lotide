a2e0d77aaee4920df1c52a580ad56ce8a792b889
Chris Hatcher <hatcherchris20@gmail.com>
2022-01-11 17:58:58 +0000

    adding the assertEqual file

---
+++ assertEqual.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(" :-) :-) Assertion Passed: (actual) === (expected)");
+  }   else {
+    console.log(":-( :-( Assertion Failed: (actual) !== (expected)");
+  }
+};
+
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+
+console.log(assertEqual("Rocket", "Rocket"));
+console.log(assertEqual(1, 2));
+
+
+
+
+
+
+
+
+/*
+Make the function compare the two values it takes in and print out a message telling us if they match or not.
+
+If the values match, it should print (console.log) the following: Assertion Passed: [actual] === [expected] (but with the values filled in)
+Otherwise it should print (console.log) the following: Assertion Failed: [actual] !== [expected] (but with the values filled in)
+Some examples:
+
+Assertion Failed: Lighthouse Labs !== Bootcamp
+Assertion Passed: 1 === 1
+Write out a few calls to it and confirm that it is indeed working as expected.
+
+Test at least the following scenarios:
+
+Comparing identical strings
+Comparing non-identical strings
+Comparing identical numbers
+Comparing non-identical numbers
+*/
