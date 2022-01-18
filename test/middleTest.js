9f2f3cb87691875de673afaac64798e65f4bc69b
Chris Hatcher <hatcherchris20@gmail.com>
2022-01-18 06:36:15 +0000

    adding middle and middleTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,27 @@
+
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-// Test assertions:
+// Mocha and Chai test assertions
 
-middle([1, 2])            // => []
-middle([1, 2, 3])         // => [2]
-middle([1, 2, 3, 4])      // => [2, 3]
+describe("#middle", () => {
+  it('returns [] for [1, 2]', () => {
+    assert.deepEqual(middle([1, 2]), [])
+  });
+  it('returns [2] for [1, 2, 3]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2])
+  });
+  it('returns [2, 3] for [1, 2, 3, 4]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3])
+  });
+});
 
+
+// Old test assertions:
+
+// middle([1, 2])            // => []
+// middle([1, 2, 3])         // => [2]
+// middle([1, 2, 3, 4])      // => [2, 3]
 
 
 
